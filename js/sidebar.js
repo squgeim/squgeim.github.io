2bea7fce0c30b52d3deb516dcd5d378dbc94eaf1
squgeim <shreyadahal@gmail.com>
2018-03-24 15:08:36 +0545

    Make sure the content remains fixed when opening and closing sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,9 +3,20 @@
 (function () {
   var menu = document.getElementById('titleMenu');
   var wrapper = document.getElementsByClassName('wrapper')[0];
+  var right = document.getElementsByClassName('right')[0];
+
+  var lastScrollPosition = 0;
 
   menu.addEventListener('click', function (e) {
-    wrapper.classList.toggle('opensidebar');
-  });
+    var currentScrollPosition = window.scrollY;
+    var wasSidebarHidden = wrapper.classList.toggle('opensidebar');
 
+    if (!wasSidebarHidden) {
+      window.scrollTo(0, lastScrollPosition); 
+      right.style.top = 0;
+    } else {
+      right.style.top = '-' + currentScrollPosition + 'px';
+      lastScrollPosition = currentScrollPosition;
+    }
+  });
 })();
